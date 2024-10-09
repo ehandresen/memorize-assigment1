eea3bf3f1f23f2abac04745be898b55df0c3f69f
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-10-09 13:08:15 +0200

    added CardView, ForEach loop, Button to add and remove cards, with protection to not crash app

--- Memorize-A1/ContentView.swift
+++ Memorize-A1/ContentView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["💪", "🏋️‍♂️", "🤸‍♂️", "🏃‍♀️", "🚴‍♀️", "🏊‍♂️"]
+    @State var count = 4
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<count, id: \.self) { index in
+                CardView(emoji: emojis[index])
+            }
+            HStack {
+                Button("add") {
+                    if count < emojis.count {
+                        count += 1
+                    }
+                }
+                Spacer()
+                Button("remove") {
+                    if count > 1 {
+                        count -= 1
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct CardView: View {
+    
+    var emoji: String = ""
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 10, style: .circular)
                 .fill(.red)
-            Text("🏋️‍♀️")
+            Text(emoji)
                 .font(.largeTitle)
         }
     }
