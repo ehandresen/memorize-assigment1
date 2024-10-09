e39080fb8fa1b494902c1d4baf7f350331279b47
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-10-09 11:46:27 +0200

    Initial Commit

---
+++ Memorize-A1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize-A1
+//
+//  Created by Eirik Andresen on 09/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize-A1/Memorize_A1App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_A1App.swift
+//  Memorize-A1
+//
+//  Created by Eirik Andresen on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_A1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
